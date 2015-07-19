6c1ce4bdf3d89abed5ca3d5734e70b5fb17ccb6a
Max DePriest <maxd924@gmail.com>
2015-07-19 00:22:18 -0500

    added the beer pair form

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -4,14 +4,39 @@ class PairsController < ApplicationController
 	end
 
 	def new
+		@pair = Pair.new
 	end
 
 	def create
+		@pair = Pair.new(pair_params)
+		@pair.user_id = current_user.id
+		@pair.save
+		# redirect_to :pairs
+	    respond_to do |format|
+	      if @pair.save
+	        format.html { redirect_to :pairs, notice: 'Beer pairing was successfully created.' }
+	        format.json { render :show, status: :created, location: @pair }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @pair.errors, status: :unprocessable_entity }
+	      end
+	    end
 	end
 
 	def show
+		@pair = Pair.find(params[:id])
+	end
+
+	def edit
+		@pair = Pair.find(params[:id])
 	end
 
 	def destroy
 	end
+
+	private
+
+	def pair_params
+      params.require(:pair).permit(:user_id, :food, :beer, :beer_type)
+    end
 end
