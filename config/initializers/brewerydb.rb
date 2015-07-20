88d25e13d6d8cc35d562433006ed22031795a25b
Max DePriest <maxd924@gmail.com>
2015-07-20 17:28:52 -0500

    fixed config file

--- config/initializers/brewerydb.rb
+++ config/initializers/brewerydb.rb
@@ -1,3 +1,3 @@
-# BreweryDB.configure do |config|
-# 	config.apikey = '5e62b4fe1d54936af2381e7885987a69'
-# end
+Rails.application.config.breweryDB.configure do |config|
+	config.apikey = '5e62b4fe1d54936af2381e7885987a69'
+end
