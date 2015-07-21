3d60bd00cd096557b68d7492dc7348dbfd97eb8d
Max DePriest <maxd924@gmail.com>
2015-07-21 07:51:01 -0500

    made a seeds file:

--- app/assets/javascripts/beer.js
+++ app/assets/javascripts/beer.js
@@ -1,75 +1,75 @@
 
 
-// $(document).ready(function() {
-//     check_beer();
-//     filter_beer();
+$(document).ready(function() {
+    check_beer();
+    filter_beer();
 
-// });
+});
 
-// function check_beer() {
-//     $('#beer-lookup').on('click', function() {
-//         $('#beer-info').text('Lame Sauce');
-//         var beer_type = $('#beer-name').val();
-//         $.ajax({
+function check_beer() {
+    $('#beer-lookup').on('click', function() {
+        $('#beer-info').text('Lame Sauce');
+        var beer_type = $('#beer-name').val();
+        $.ajax({
     
-//             url: '/proxy?url=https://api.brewerydb.com/v2/?key=5e62b4fe1d54936af2381e7885987a69&name=' + beer_type,
-//             contentType: "application/json",
-//             dataType: 'json',
-//             type: 'get',
+            url: '/proxy?url=https://api.brewerydb.com/v2/?key=5e62b4fe1d54936af2381e7885987a69&name=' + beer_type,
+            contentType: "application/json",
+            dataType: 'json',
+            type: 'get',
 
-//             success: function(data) {
-//                 alert('Test');
-//                 console.log(data);
-//             },
+            success: function(data) {
+                alert('Test');
+                console.log(data);
+            },
 
-//             error: function() {
-//                 alert('No dice');
-//             }
-//         });
-//     });
+            error: function() {
+                alert('No dice');
+            }
+        });
+    });
 
-// }
+}
 
-// function filter_beer() {
-//     $('#beer-selector').change(function() {
-//         var choice = $('#beer-selector').val();
-//         $('.hidden').val(choice);
-//         $.ajax({
-//             url: '/',
-//             method: 'GET',
-//             dataType: 'json',
+function filter_beer() {
+    $('#beer-selector').change(function() {
+        var choice = $('#beer-selector').val();
+        $('.hidden').val(choice);
+        $.ajax({
+            url: '/',
+            method: 'GET',
+            dataType: 'json',
 
-//             success: function(data) {
-//                 console.log(data);
-//             },
+            success: function(data) {
+                console.log(data);
+            },
 
-//             error: function(jqXHR, textStatus, errorThrown) {
-//                 console.log(textStatus + "    " + errorThrown);
-//             }
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus + "    " + errorThrown);
+            }
 
-//         });
-//     });
-// }
+        });
+    });
+}
 
-// $('#beer-selector').change(function() {
-//         var choice = $('#beer-selector').val();
-//         $('.hidden').val(choice);
-//         $.ajax({
-//             url: '/',
-//             method: 'GET',
-//             dataType: 'json',
+$('#beer-selector').change(function() {
+        var choice = $('#beer-selector').val();
+        $('.hidden').val(choice);
+        $.ajax({
+            url: '/',
+            method: 'GET',
+            dataType: 'json',
 
-//             success: function(data) {
-//                 console.log(data);
-//             },
+            success: function(data) {
+                console.log(data);
+            },
 
-//             error: function(jqXHR, textStatus, errorThrown) {
-//                 console.log(textStatus + "    " + errorThrown);
-//             }
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus + "    " + errorThrown);
+            }
 
-//         });
-// });
+        });
+});
 
 
 
-// // Beer API key:  5e62b4fe1d54936af2381e7885987a69
+// Beer API key:  5e62b4fe1d54936af2381e7885987a69
